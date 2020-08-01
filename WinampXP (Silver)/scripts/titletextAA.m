9b9fc38d0d7c682e92937e2e96f142d3a2360a44
mirzi <60480862+mirzi1@users.noreply.github.com>
2020-08-01 19:48:49 +0200

    silver skin drop shadow is no longer ugly

--- WinampXP (Silver)/scripts/titletextAA.m
+++ WinampXP (Silver)/scripts/titletextAA.m
@@ -3,29 +3,41 @@
 //guess we are doing this the hard way
 //include this in skin.xml if the title shadow looks like ass
 
-Global Container containerMain, /*containerEQ*/;
-Global Layout layoutMainNormal, layoutMainShade;
-Global Group MainGroup, MainGroupShade, /*EQGroup*/;
-Global Text MainTitle, ShadeTitle;
+Global Container containerMain, containerPL , containerEQ;
+Global Layout layoutMainNormal, layoutMainShade, layoutPLNormal, layoutPLShade, layoutEQShade;
+Global Group MainGroup, MainGroupShade, PLGroupShade, PLGroupShadeDisplay, EQShadeGroup;
+Global Text MainTitle, ShadeTitle, PLEditInfo, EQShadeTitle;
+Global GuiObject PLShadeTitle;
 
 System.onScriptLoaded(){
   containerMain = System.getContainer("main");
 	layoutMainNormal = containerMain.getLayout("normal");
 	MainGroup = layoutMainNormal.getObject("player.normal.group.main");
-
   layoutMainShade = containerMain.getLayout("shade");
   MainGroupShade = layoutMainShade.getObject("player.shade.group.main");
 
-  /*
+  containerPL = System.getContainer("PLEdit");
+  layoutPLShade = containerPL.getLayout("shade");
+  layoutPLNormal = containerPL.getLayout("normalpl");
+  PLGroupShade = layoutPLShade.getObject("pledit.shade.group.main");
+  PLGroupShadeDisplay = PLGroupShade.getObject("pledit.shade.group.display");
+
   containerEQ = System.getContainer("equalizer");
-  EQGroup = containerEQ.getObject("equalizer.normal.group.main.content");
-  //yeah i kinda just gave up here
-  */
+  layoutEQShade = containerEQ.getLayout("shade");
+  EQShadeGroup = layoutEQShade.getObject("equalizer.shade.group");
 
   MainTitle = MainGroup.getObject("window.titlebar.title.dropshadow");
   ShadeTitle = MainGroupShade.getObject("shade.window.titlebar.title.dropshadow");
+  PLShadeTitle = PLGroupShadeDisplay.getObject("main.pl.shadow");
+  PLEditInfo = layoutPLNormal.getObject("pledit.info.shadow");
+  EQShadeTitle = EQShadeGroup.getObject("window.titlebar.title.dropshadow");
+
+  //all this because of an ugly drop shadow
 
   MainTitle.setXmlParam("antialias", "0");
   ShadeTitle.setXmlParam("antialias", "0");
+  PLShadeTitle.setXmlParam("antialias", "0");
+  PLEditInfo.setXmlParam("antialias", "0");
+  EQShadeTitle.setXmlParam("antialias", "0");
 
   }
