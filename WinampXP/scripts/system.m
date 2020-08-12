bed2cfc6b1c8a3c3d40522fdc353861c8bce60d8
mirzi <60480862+mirzi1@users.noreply.github.com>
2020-08-12 12:55:22 +0200

    ghostbusters

--- WinampXP/scripts/system.m
+++ WinampXP/scripts/system.m
@@ -5,7 +5,6 @@
 #include "global.m"
 #include "mainplayer.m"
 #include "shadeplayer.m"
-#include "seek_slider.m"
 #include "songticker.m"
 
 
@@ -14,7 +13,6 @@ System.onScriptLoaded() {
 	initMainPlayer();
 	initShadePlayer();
 	init_Songticker();
-	initSeekSlider();
 }
 
 System.onScriptUnloading() {
